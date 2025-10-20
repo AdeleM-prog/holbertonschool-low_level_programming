6421313151d43f64a08b5b64f8280b0ac3f182e4
AdeleM-prog <12043@holbertonstudents.com>
2025-10-20 16:58:06 +0000

    change 4-

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/**C program displaying a string
-*main: main function of the program, returning int
-*return (0): everything worked correctly !**/
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
