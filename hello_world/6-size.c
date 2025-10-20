1b295398f16fa0cbb1fbe58f410f5bcd3e12f910
AdeleM-prog <12043@holbertonstudents.com>
2025-10-20 18:00:36 +0000

    add 6-

---
+++ hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+{
+$MYSIZE = "./size32"
+
+  if ($MYSIZE) {
+int printf("Size of a char: 1 byte(s)\n");
+int printf("Size of an int: 4 byte(s)\n");
+int printf("Size of a long int: 4 byte(s)\n");
+int printf("Size of a long long int: 8 byte(s)\n");
+int printf("Size of a float: 4 byte(s)\n");
+}
+  else {
+int printf("Size of a char: 1 byte(s)\n");
+int printf("Size of an int: 4 byte(s)\n");
+int printf("Size of a long int: 8 byte(s)\n");
+int printf("Size of a long long int: 8 byte(s)\n");
+int printf("Size of a float: 4 byte(s)\n");
+}
+ return (0);
+}
