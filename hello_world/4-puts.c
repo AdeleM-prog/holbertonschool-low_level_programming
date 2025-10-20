dd2e783fbe0acced20fdf63671c516fda0633e54
AdeleM-prog <12043@holbertonstudents.com>
2025-10-20 15:10:28 +0000

    change 4-

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ *int: returning 0 : saying everything is fine
+ *main: main function of the program
+ *void: no arguments
+ *puts: function to display a string
  */
 int main (void)
 {
-  puts("/"Programming is like building a multilingual puzzle");
+  puts( "\"Programming is like building a multilingual puzzle" );
        return (0);
 }
