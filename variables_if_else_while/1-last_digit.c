264cadf2c97ac370321a775f9b460798f09ac5d0
AdeleM-prog <12043@holbertonschool.com>
2025-10-21 09:38:04 +0000

    add 1- + betty

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio>
+
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int m = n % 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 5)
+printf("Last digit of %ld is %d and is greater than 5\n", n, m);
+else if (n == 0)
+printf("Last digit of %ld is %d and is 0", n, m);
+else
+printf("Last digit of %ld is %d and is less than 6 and not 0", n m);
+return (0);
+}
