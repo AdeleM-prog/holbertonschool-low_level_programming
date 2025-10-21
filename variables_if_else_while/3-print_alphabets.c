a28708450664500b10acd64ac189be3f22ed168b
AdeleM-prog <12043@holbertonschool.com>
2025-10-21 12:17:21 +0000

    add 3-

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+{
+int i = 97;
+int j = 65;
+for (; i <= 122 ; i++)
+{
+putchar(i);
+}
+for (; j <=90 ; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
