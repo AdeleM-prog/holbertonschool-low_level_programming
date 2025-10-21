220396a22bf0b9b20c0083edbc4ef2ecbd85ee7a
AdeleM-prog <12043@holbertonschool.com>
2025-10-21 08:30:51 +0000

    add 0- + betty

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio>
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+n;
+if (n > 0)
+printf("%d is positive\n");
+else
+printf("%d is negative\n");
+return (0);
