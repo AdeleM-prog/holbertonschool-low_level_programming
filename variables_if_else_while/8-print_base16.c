e49615e0674a90fda2550935c259a0aa45031860
AdeleM-prog <12043@holbertonschool.com>
2025-10-21 13:15:01 +0000

    add 8-

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+{
+int i = 48;
+int j = 97;
+for (; i <= 57 ; i++)
+{
+putchar(i);
+}
+for (; j <= 102 ; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
