3685be13f93ad1b6f13270caacdbaff872a9cbde
AdeleM-prog <12043@holbertonschool.com>
2025-10-21 13:03:01 +0000

    add 7-

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+{
+int i = 122;
+for (; i <= 97 ; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
