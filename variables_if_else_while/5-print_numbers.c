e1f746e23bb2e87c568ec4ceec564e341c0c09bc
AdeleM-prog <12043@holbertonschool.com>
2025-10-21 12:48:31 +0000

    add 5-

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  affiche un message en sortie
+ * Return: 0
+ */
+int main(void)
+{
+int i = 0;
+for (; i <= 10 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
