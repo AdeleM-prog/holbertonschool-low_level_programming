e33977f744e6c401837437cad1612644badf9bbe
AdeleM-prog <12043@holbertonschool.com>
2025-10-22 12:14:45 +0000

    change 0- betty

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Description: This function prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
