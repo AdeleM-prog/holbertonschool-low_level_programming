812aa7bb4f662383f1126e9264f48def1671edbd
AdeleM-prog <12043@holbertonschool.com>
2025-10-23 18:50:23 +0000

    change -4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * _islower - checks if a character is lowercase
+ * _isalpha - checks if a character is lowercase
  * @c: character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is lowercase or uppercase, 0 otherwise
  */
-int _islower(int c)
+int _isalpha(int c)
 {
   if (c >= 65 && c <= 122 && !(c >= 91 && c<=96))
 return (1);
