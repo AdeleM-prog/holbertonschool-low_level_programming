79093a376c57f0026fa79a9d3a818669aea40ef9
AdeleM-prog <12043@holbertonschool.com>
2025-10-23 20:19:28 +0000

    add 7-

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_sign - prints sign of a number
+ * @n: number to check
+ * Return: 1 if n greater than 0, return 0 if equal 0,
+ * return -1 if less than zero
+ */
+int print_last_digit()
+int n;
+int m= n % 10;
+_putchar('m');
+return (m);
