8b59f3c7c5124eb9d2cac84cc582ede45bf14558
AdeleM-prog <12043@holbertonschool.com>
2025-10-23 19:57:49 +0000

    add 6- + betty

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _abs - computes absolute value of a number
+ * @n: number to check
+ * Return: n if n greater than 0, return -n if less than 0
+ */
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-n);
+}
+}
