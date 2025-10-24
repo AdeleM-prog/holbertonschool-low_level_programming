301eb5baa070c55eddecade7e271cb4eaaf0302c
AdeleM-prog <12043@holbertonschool.com>
2025-10-24 10:06:56 +0000

    add 9-

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ * 
+ * Return: void
+ */
+void times_table(void)
+int a = 48;
+int b = 49;
+int c = 50;
+int d = 51;
+int e = 52;
+int f = 53;
+int g = 54;
+int h = 55;
+int i = 56;
+int j = 57;
+int multiplicateur;
+for (multiplicateur = 48; multiplicateur <=9; multiplicateur++)
+{
+_putchar(a', ');
+_putchar('\n');
+_putchar(b', ');
+_putchar('\n');
+_putchar(c', ');
+_putchar('\n');
+_putchar(d', ');
+_putchar('\n');
+_putchar(e', ');
+_putchar('\n');
+_putchar(f', ');
+_putchar('\n');
+_putchar(g', ');
+_putchar('\n');
+_putchar(h', ');
+_putchar('\n');
+_putchar(i', ');
+_putchar('\n');
+_putchar(j);
+}
