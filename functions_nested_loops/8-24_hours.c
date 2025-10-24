ab6a4d19500f4a9a6337707a174584f7b2a7a31e
AdeleM-prog <12043@holbertonschool.com>
2025-10-24 09:31:43 +0000

    add 8- + betty

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ * Return: void
+ */
+void jack_bauer(void)
+{
+char i;
+char j;
+char k;
+char l;
+for (l = '0'; l <= '2'; l++)
+{
+for (k = '0'; k <= '9'; k++)
+{
+if (l == '2' && k > '3')
+{
+break;
+}
+for (j = '0'; j <= '5'; j++)
+{
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(l);
+_putchar(k);
+_putchar(':');
+_putchar(j);
+_putchar(i);
+_putchar('\n');
+}
+}
+}
+}
+}
+}
