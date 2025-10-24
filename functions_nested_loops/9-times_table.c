b6da643d61691c8c5f867781bb357171a99484a8
AdeleM-prog <12043@holbertonstudents.com>
2025-10-24 12:14:58 +0000

    add 9-

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,36 +5,34 @@
  * Return: void
  */
 void times_table(void)
-int a = 48;
-int b = 49;
-int c = 50;
-int d = 51;
-int e = 52;
-int f = 53;
-int g = 54;
-int h = 55;
-int i = 56;
-int j = 57;
-int multiplicateur;
-for (multiplicateur = 48; multiplicateur <=9; multiplicateur++)
 {
-_putchar(a', ');
+int a = 0;
+int b = 0;
+int x = 0;
+for (; a <= '9' ; a++)
+{
+for (; b <= '9' ; b++)
+{
+x = a*b;
+if (b != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+ if (x <= '9')
+{
+_putchar(' ');
+}
+if (x <= '9')
+{
+_putchar('0' + x);
+}
+else
+{
+_putchar('0' + (x/10));
+_putchar('0' + (x % 10));
+}
+}
 _putchar('\n');
-_putchar(b', ');
-_putchar('\n');
-_putchar(c', ');
-_putchar('\n');
-_putchar(d', ');
-_putchar('\n');
-_putchar(e', ');
-_putchar('\n');
-_putchar(f', ');
-_putchar('\n');
-_putchar(g', ');
-_putchar('\n');
-_putchar(h', ');
-_putchar('\n');
-_putchar(i', ');
-_putchar('\n');
-_putchar(j);
+}
 }
