af12ac354d278f3d513334a8eb608078bdc71911
AdeleM-prog <12043@holbertonschool.com>
2025-10-27 08:54:49 +0000

    add 0- + betty + main.h

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+/**
+ * _isupper - function that checks for uppercase character.
+ * @c: variable to check
+ * Return: 1 if uppercase, otherwise 0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
