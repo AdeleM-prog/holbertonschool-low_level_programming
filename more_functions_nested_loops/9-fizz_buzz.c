ef351c2e5686946b1a9f062a432af53822ac8963
AdeleM-prog <12043@holbertonschool.com>
2025-10-27 15:50:27 +0000

    add 9- + betty

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - function that prints the numbers from 1 to 100
+ *
+ * Return: void
+ */
+int main(void)
+{
+int a = 1;
+for (; a <= 100 ; a++)
+{
+if (a % 3 == 0)
+{
+printf("Fizz");
+}
+else if (a % 5 == 0)
+}
+printf("Buzz");
+}
+else
+{
+printf("%d", a);
+}
+}
