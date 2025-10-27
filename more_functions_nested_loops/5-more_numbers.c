1410313c7f10930772d42d14c99e0cde8ac4a814
AdeleM-prog <12043@holbertonschool.com>
2025-10-27 10:36:41 +0000

    change 5-

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints the numbers,
+ * from 0 to 14, followed by a new line.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+  int i;
+  int j;
+  for (; i <= 10; i++)
+    {
+      for (; j
