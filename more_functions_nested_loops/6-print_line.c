246ac9f5c7a8462937849520bb14fa81b55888f0
AdeleM-prog <12043@holbertonschool.com>
2025-10-27 12:57:05 +0000

    add 6- + bettyy

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - function that prints a line
+ *@n: the number of times the character _ should be printed
+ * Return: void
+ */
+void print_line(int n)
+{
+for (; n > 0;)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
