ea2b9bfb3a34686002c5b26d4e9998ef7e473e68
AdeleM-prog <12043@holbertonstudents.com>
2025-10-28 08:03:53 +0000

    change 9-

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 int a = 1;
 for (; a <= 100 ; a++)
 {
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz ");
+}
 if (a % 3 == 0)
 {
 printf("Fizz ");
 }
-else if (a % 5 == 0)
+if (a % 5 == 0)
 {
 printf("Buzz ");
-}
-else if (a % 3 == 0 && a % 5 == 0)
-{
-printf("FizzBuzz ");
 }
 else
 {
