3d434d1deefea0009623f1a879264add40ef19ec
AdeleM-prog <12043@holbertonstudents.com>
2025-10-28 08:07:30 +0000

    change 9-

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ if (a % 3 == 0 && a % 5 == 0)
 {
 printf("FizzBuzz ");
 }
-if (a % 3 == 0)
+else if (a % 3 == 0)
 {
 printf("Fizz ");
 }
-if (a % 5 == 0)
+else if (a % 5 == 0)
 {
 printf("Buzz ");
 }
