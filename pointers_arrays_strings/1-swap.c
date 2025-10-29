67e0c7303c7f63be3554aab44a64d21516ad6192
AdeleM-prog <12043@holbertonstudents.com>
2025-10-29 08:07:24 +0000

    add 1- + betty

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two integers
+ * @a: int to swap
+ * @b: int to swap
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+int temp = 0;
+int a;
+int b;
+*a = temp;
+*a = *b;
+*b = temp;
+}
