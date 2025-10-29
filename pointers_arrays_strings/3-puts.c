f51fe74a0ad762c9600cb1115306407d19eed99f
AdeleM-prog <12043@holbertonstudents.com>
2025-10-29 09:39:55 +0000

    add 3- + betty

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+int i = 0;
+for (; str[i] != '\0' ; i++)
+{
+_putchar(str[i]);
+}
+}
