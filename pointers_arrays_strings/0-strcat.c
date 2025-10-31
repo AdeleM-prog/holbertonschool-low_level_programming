e07197b642e0a91ab156eabd08f2cf50befedb42
AdeleM-prog <12043@holbertonstudents.com>
2025-10-31 09:01:39 +0000

    add 0-strcat + betty

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @src: string to copy
+ * @dest: destination file
+ * Return: string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i = 0;
+int j = 0;
+for (; dest[i] != '\0'; i++)
+{
+}
+for (; src[j] != '\0'; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
