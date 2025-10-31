3dd56d0f679e718063995f4c2b5d79120a3ddb27
AdeleM-prog <12043@holbertonstudents.com>
2025-10-31 10:03:53 +0000

    add 1-strn + betty

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings
+ * @src: string to copy
+ * @dest: destination file
+ * @n: number of bytes
+ * Return: string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+for (; dest[i] != '\0'; i++)
+{
+}
+while (j < n && src[j] != '\0')
+}
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
