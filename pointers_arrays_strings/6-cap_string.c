25f6860c184d157d0fd7164aa79fd2bb6cb05846
AdeleM-prog <12043@holbertonstudents.com>
2025-11-02 16:17:45 +0000

    add 6- + betty

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: string to check
+ * Return: char
+ */
+char *cap_string(char *s)
+{
+int i;
+char sep[] = " \t\n,;.!, ?\"(){}";
+int j;
+if (s[i] >= 97 && s[i] <= 122)
+{
+s[i] = s[i] - 32;
+}
+for {i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j != '\0'; j++)
+{
+if (s[i] == sep[j] && s[i + 1] >= 97 || s[i + 1] <= 122)
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
+}
+return (s[i]);
+}
