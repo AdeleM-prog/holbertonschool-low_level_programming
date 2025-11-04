9f2712c54a3baf21daa6cb8b019928b2915ac814
AdeleM-prog <12043@holbertonstudents.com>
2025-11-04 13:56:12 +0000

    add 7-print + bettyy

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ *@a: pointer to a table of 8 chars
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+int i;
+int j;
+for (i = 0; i <= 7; i++)
+{
+for (j = 0; j <= 7; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
