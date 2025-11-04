597f6116d6be2f7eddefd4cd9b35178ddc0acb8a
AdeleM-prog <12043@holbertonstudents.com>
2025-11-04 09:56:22 +0000

    add 4-str + betty

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - check the code
+ * @s : string to check
+ * @accept: bytes to compare
+ * Return: &s[i] if match, NULL if no match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+int j;
+char *found = NULL;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+found = &s[i];
+return (found);
+}
+}
+}
+return (found);
+}
