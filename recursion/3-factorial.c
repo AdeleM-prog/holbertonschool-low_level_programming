664014e0e3e12d79aa1ccefeb976ecc62da6b621
AdeleM-prog <12043@holbertonstudents.com>
2025-11-05 14:16:27 +0000

    change 3- + betty

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: number factorial
+ * Return: -1 if error
+ */
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+{
+return (1);
+}
+else
+n = n *factorial(n - 1);
+return (n);
+}
