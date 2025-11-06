74822012ee1d88f8acbcf5521354cffb87df11a6
AdeleM-prog <12043@holbertonstudents.com>
2025-11-06 20:57:33 +0000

    change 6- + betty

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,10 +1,32 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * is_prime_number - returns 1 if integer is prime number
+ * @n: number to check
+ * Return: returns 1 if integer is a prime number, otherwise return 0
  */
 int is_prime_number(int n)
 {
+if (n <= 1)
+return (0);
+if (n == 2)
+return (1);
+if (n % 2 == 0 && n > 2)
+return (0);
+else
+return (is_prime_helper(n, 3));
+}
 
+/**
+ * is_prime_helper - recursion
+ * @n: number to check
+ * @i: iterations
+ * Return: returns 1 if integer is a prime number, otherwise return 0
+ */
+int is_prime_helper(int n, int i)
+{
+if (i * i > n)
+return (1);
+if (n % i == 0)
+return (0);
+return (is_prime_helper(n, i + 2));
 }
