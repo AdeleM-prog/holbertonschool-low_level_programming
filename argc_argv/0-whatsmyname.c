a6a53a8851f24dea4af14143b47657fe68f70c82
AdeleM-prog <12043@holbertonstudents.com>
2025-11-07 09:34:34 +0000

    change 0- + betty

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: string to print
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s", argv[0]);
+printf("\n");
+return (0);
+}
