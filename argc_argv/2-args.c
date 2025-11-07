2fbd5add26344a7a314808bf8c1a1cd7a9ea345d
AdeleM-prog <12043@holbertonstudents.com>
2025-11-07 10:06:27 +0000

    add 2- + betty

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: string to print
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
