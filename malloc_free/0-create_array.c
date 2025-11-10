8dc22cce80f4c01414978283e9ed3eb85df26515
AdeleM-prog <12043@holbertonstudents.com>
2025-11-10 13:24:25 +0000

    main.h _putchar

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates array of chars
+ * @c: char to initialize the array
+ * @size: the size of the memory to print
+ *
+ * Return: pointer to the array, NULL if size = 0 or fail
+ */
+char *create_array(unsigned int size, char c)
+{
+
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN
+#define MAIN
+
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+
+
+#endif
