c6cb5b36fed9ac956d6d7d232b9902cf16c06878
AdeleM-prog <12043@holbertonstudents.com>
2025-11-14 12:14:02 +0000

    add 5- + betty

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include "main.h"
+/**
+ * free_dog - function that frees the dogs
+ * @d: pointer to the structure
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return (NULL);
+else
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
