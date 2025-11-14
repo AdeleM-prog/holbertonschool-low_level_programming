962ae735dc46f91119f2a7747d6797e55378fd0c
AdeleM-prog <12043@holbertonstudents.com>
2025-11-14 11:59:51 +0000

    change 4-

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,26 +12,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
-unsigned int i;
+int i;
+int size1;
+int size2;
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
-new_dog->name = malloc(strlen(name) + 1);
+for (size1 = 0; name[size1] != '\0'; size1++)
+{
+}
+new_dog->name = malloc(size1 + 1);
 if (new_dog->name == NULL)
 {
 free(new_dog);
 return (NULL);
 }
-for (i = 0; i <= strlen(name); i++)
+for (i = 0; i <= size1; i++)
 new_dog->name[i] = name[i];
-new_dog->owner = malloc(strlen(owner) + 1);
+for ( size2 = 0; owner[size2] != '\0'; size2++)
+{
+}
+new_dog->owner = malloc(size2 + 1);
 if (new_dog->owner == NULL)
 {
 free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
-for (i = 0; i <= strlen(owner); i++)
+for (i = 0; i <= size2; i++)
 new_dog->owner[i] = owner[i];
 new_dog->age = age;
 return (new_dog);
