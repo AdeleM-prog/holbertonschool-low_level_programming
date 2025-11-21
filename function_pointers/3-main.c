3bf4356450b7b9e4de9d5bb1c89a8f8d966c37b6
AdeleM-prog <12043@holbertonstudents.com>
2025-11-21 16:19:16 +0000

    corrections + comp + betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
- * struct op - Struct op
+ * main - main function
  *
- * @op: The operator
- * @f: The function associated
+ * Return: int
  */
-int main (void)
+int main(int argc, char *argv[])
 {
+int a;
+int b;
+int result;
+int (*op_func)(int, int);
 
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+op_func = get_op_func(argv[2]);
+if (op_func == NULL)
+{
+printf("Error\n");
+return (99);
+}
+if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+{
+printf("Error\n");
+return (100);
+}
+result = op_func(a, b);
+printf("%d\n", result);
+return (0);
 }
