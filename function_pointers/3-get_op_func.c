6434b31202233f222e81fb6605d60d0c6be995f2
AdeleM-prog <12043@holbertonstudents.com>
2025-11-21 15:41:14 +0000

    3-get_op_func + betty

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
- * struct op - Struct op
+ * get_op_func - determines function to use
  *
- * @op: The operator
- * @f: The function associated
+ * @s: string to check
+ * Return: function to use, NULL if fails
  */
 
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-    if ()
-      {
-
-      }
-  
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i = 0;
+while (ops[i].op != NULL)
+{
+if (s[0] == ops[i].op && s[1] == '\0')
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
 }
