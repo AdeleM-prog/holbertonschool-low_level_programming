6282092c3f3989436481120963e4c12cbb6acf00
AdeleM-prog <12043@holbertonstudents.com>
2025-11-21 11:02:29 +0000

    3-op_functions.c + betty

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,5 +1,61 @@
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+#include "3-calc.h"
+
+/**
+ * op_add - sum of 2 ints
+ *
+ * @a: int to add
+ * @b: int to add
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - difference of 2 ints
+ *
+ * @a: int to substract
+ * @b: int to substract
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - poduct of 2 ints
+ *
+ * @a: int to multiply
+ * @b: int to mulitply
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - division of 2 ints
+ *
+ * @a: int to divide
+ * @b: int to divide
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - remainer of division
+ *
+ * @a: int to divide
+ * @b: int to divide
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
