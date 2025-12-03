62d98903001f5e6038e7190d8c2460a60dc2bebe
AdeleM-prog <12043@holbertonstudents.com>
2025-12-03 18:18:09 +0000

    correction 3-a + betty + comp

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,23 +2,26 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * add_dnodeint_end - adds a node at the end
+ * @head: pointer to a pointer pointing to the head
+ * @n: content of the node
+ * Return: address of the new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-  dlistint_t *new, *h = *head;
-  new = malloc (sizeof(dlistint_t));
-  if (new == NULL)
-    return (NULL);
-  new->n = n;
-  new->prev = *head;
-  new->next = NULL;
-  while (h->next != NULL)
-    {
-      h = h->next;
-	}
-  h->next = new;
-  return (new);
+dlistint_t *new, *h = *head;
+if (*head == NULL)
+return (NULL);
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
+new->n = n;
+new->prev = *head;
+new->next = NULL;
+while (h->next != NULL)
+{
+h = h->next;
+}
+h->next = new;
+return (new);
 }
