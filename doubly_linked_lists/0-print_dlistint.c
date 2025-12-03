6bdd5054dac9d498de07b3bbe5f6799d336a51cc
Your Name <12043@holbertonstudents.com>
2025-12-03 16:54:47 +0000

    crea 0-p + betty + comp

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,10 +1,20 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 /**
- * main - check the code
- *
- * Return: Always EXIT_SUCCESS.
+ * print_dlistint - prints all elements of a dlistint_t list
+ * @h: pointer to the node to print
+ * Return: nb of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  
+size_t nbnodes = 0;
+while (h != NULL)
+{
+printf("%u\n", h->n);
+nbnodes++;
+h = h->next;
+}
+return (nbnodes);
 }
